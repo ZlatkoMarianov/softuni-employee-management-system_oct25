d67d7f80c6c91190f4baf575fce6a8ef028bac5b
ZlatkoMarianov <zlatkomarianov@abv.bg>
2025-11-10 22:12:51 +0200

    Refactor user data fetching to App component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Footer from './components/Footer.jsx';
 import Header from './components/Header.jsx';
 import Pagination from './components/Pagination.jsx';
@@ -8,6 +8,17 @@ import CreateUserModal from './components/CreateUserModal.jsx';
 
 function App() {
   const [showCreateUser, setShowCreateUser] = useState(false);
+  const [users, setUsers] = useState([]);
+  const [forceRefresh, setForceRefresh] = useState(true);
+
+  useEffect(() => {
+    fetch('http://localhost:3030/jsonstore/users')
+      .then((response) => response.json())
+      .then((result) => {
+        setUsers(Object.values(result));
+      })
+      .catch((err) => alert(err.message));
+  }, [forceRefresh]);
 
   const addUserClickHandler = () => {
     setShowCreateUser(true);
@@ -19,6 +30,32 @@ function App() {
 
   const AddUserHandlerSubmit = (e) => {
     e.preventDefault();
+
+    const formData = new FormData(e.target);
+
+    const { country, city, street, streetNumber, ...userData } = Object.fromEntries(formData);
+    userData.address = {
+      country,
+      city,
+      street,
+      streetNumber,
+    };
+
+    userData.createdAt = new Date().toISOString();
+    userData.updatedAt = new Date().toISOString();
+
+    fetch('http://localhost:3030/jsonstore/users', {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json',
+      },
+      body: JSON.stringify(userData),
+    })
+      .then(() => {
+        closeUserModalHandler();
+        setForceRefresh((state) => !state);
+      })
+      .catch((err) => alert(err.message));
   };
 
   return (
@@ -29,7 +66,7 @@ function App() {
         <section className="card users-container">
           <Search />
 
-          <UserList />
+          <UserList users={users} />
 
           <button className="btn-add btn" onClick={addUserClickHandler}>
             Add new user
