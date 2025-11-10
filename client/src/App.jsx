28c0e3f787a598648641e2200ab66b05673dc3f0
ZlatkoMarianov <zlatkomarianov@abv.bg>
2025-11-10 19:00:57 +0200

    Add user list UI components and restructure styles

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,35 @@
+import Footer from './components/Footer.jsx';
+import Header from './components/Header.jsx';
+import Pagination from './components/Pagination.jsx';
+import Search from './components/Search.jsx';
+import UserList from './components/UserList.jsx';
+
+
 function App() {
   return (
-    <div>
-      <h1>Hello</h1>
-    </div>
+    <main>
+      <Header />
+
+      <main className="main">
+        <section className="card users-container">
+          <Search />
+
+          <UserList />
+
+          <button className="btn-add btn">Add new user</button>
+
+          <Pagination />
+        </section>
+
+        {/* User details component  */}
+
+        {/* Create/Edit Form component  */}
+
+        {/* Delete user component  */}
+      </main>
+
+      <Footer />
+    </main>
   );
 }
 
